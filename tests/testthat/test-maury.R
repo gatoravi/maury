2ebcdf1eed38ee1625bb4a2b5eecbbe871525b2f
Avinash Ramu <aramu@genome.wustl.edu>
2015-04-14 10:00:36 -0500

    update test

--- tests/testthat/test-maury.R
+++ tests/testthat/test-maury.R
@@ -1,10 +1,13 @@
 context("maury")
 
-test_that("check no read-count sites", {
-    f1 <- system.file("extdata", 'ex1.bam',
+test_that("check no genotypes case", {
+    bam <- system.file("extdata", 'ex1.bam',
         package = "maury", mustWork = TRUE)
     vcf <- system.file("extdata", 'ex1.vcf',
         package = "maury", mustWork = TRUE)
-    expect_equal(maury(vcf, f1, f1), c(1))
+    expect_equal(maury("test_sample", vcf, bam, bam,
+                        min_rd = 0, min_mq = 0, min_bq = 0),
+                 list(sample = "test_sample", matches = 1,
+                    total = 1, score = 1))
 })
 
